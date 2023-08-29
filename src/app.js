97c3a43b76551dc3fb4f5553b6368aa5c5a8b84a
Matias Portal <portalmatias4@gmail.com>
2023-08-29 20:26:58 -0300

    Cambio url mongoose

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
+import adoptionsRouter from './routes/adoption.router.js';
+import cookieParser from 'cookie-parser';
 import express from 'express';
 import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
-
-import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
-import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+import usersRouter from './routes/users.router.js';
 
 const app = express();
 const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(`URL DE MONGO`)
+const connection = mongoose.connect("mongodb+srv://coder51220:coder2023@cluster0.4qaobt3.mongodb.net/coder51220")
 
 app.use(express.json());
 app.use(cookieParser());
